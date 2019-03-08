013992710b8c18c77edace53f3b215824e9163dd
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-03-08 21:16:38 +0200

    Update index.html and server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var http = require("http");
+var fs = require("fs");
 
 var host = "127.0.0.1";
 var port = "3000";
 
-var server = http.createServer(function(request, response) {
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("hi world");
-});
+fs.readFile("./index.html", function(error, html) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  var server = http.createServer(function(request, response) {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html");
+    response.write(html);
+    response.end();
+  });
 
-server.listen(port, host, function() {
-  console.log("Server is listening on port " + port);
+  server.listen(port, host, function() {
+    console.log("Server is running on port " + port);
+  });
 });
