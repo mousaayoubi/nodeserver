7ebfed1633b57799f60eb9aa9b17bd3432f1756e
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-03-11 17:01:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ var fs = require("fs");
 var host = "127.0.0.1";
 var port = "3000";
 
-fs.readFile("./index.html", function(error, html) {
+fs.readFile("./index.html", function(error, data) {
   if (error) {
     console.log(error);
     return;
   }
   var server = http.createServer(function(request, response) {
     response.statusCode = 200;
-    response.setHeader("Content-Type", "text/html");
-    response.write(html);
+    response.getHeader("Content-Type", "text/html");
+    response.write(data);
     response.end();
   });
 
   server.listen(port, host, function() {
-    console.log("Server is running on port " + port);
+    console.log("Server is listening on port " + port);
   });
 });
